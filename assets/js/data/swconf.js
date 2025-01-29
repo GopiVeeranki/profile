581bb923a505a761e205b002cdc98682b3773cf6
veeranki.gopi <veeranki.gopi@servicenow.com>
2025-01-29 15:03:11 +0530

    TEST: First commit

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'lumos-1738142801',resources: [
+      '/pages/dev/lumos/assets/css/jekyll-theme-lumos.css',
+      '/pages/dev/lumos/',
+      
+        '/pages/dev/lumos/categories/',
+      
+        '/pages/dev/lumos/tags/',
+      
+        '/pages/dev/lumos/archives/',
+      
+        '/pages/dev/lumos/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
